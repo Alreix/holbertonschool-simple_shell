de5696e01b0ff9ad7b3ad742c4518a6ed33e9ae6
Alreix <alreixdev@gmail.com>
2025-12-20 14:09:19 +0100

    feat: create shell.h with prototypes for simple shell 0.1

--- shell.h
+++ shell.h
@@ -1,5 +1,13 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int print_prompt(void);
+int fork_and_execute_command(char *cmd, char **env, char *progname);
 
 #endif
