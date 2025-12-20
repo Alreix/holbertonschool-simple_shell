7e1fc6877b828960d94bf727289ab58bc6396248
Alreix <alreixdev@gmail.com>
2025-12-20 18:02:50 +0100

    feat: update shell.h with parsing and execution prototypes

--- shell.h
+++ shell.h
@@ -8,6 +8,10 @@
 #include <sys/types.h>
 
 int print_prompt(void);
-int fork_and_execute_command(char *cmd, char **env, char *progname);
+
+char **split_line(char *line, char *separator);
+void free_tokens(char **tokens);
+
+int fork_and_execute_command(char **argv, char **env, char *progname);
 
 #endif
