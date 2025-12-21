af9ad70b2507a84018ecaa135c1a62a9a20329b4
Alreix <alreixdev@gmail.com>
2025-12-21 23:46:30 +0100

    feat: modification of the fork_and_execute_cmd function (addition of the parameter to count the number of lines and interactive to return to the print_not_found function)

--- execute.c
+++ execute.c
@@ -7,15 +7,18 @@
 #include <errno.h>
 
 /**
- * fork_and_execute_command - forks a child process and executes a command
+ * fork_and_execute_cmd - forks a child process and executes a command
  * @argv: argument vector (argv[0]is the command path)
  * @env: environment variables
  * @progname: name of the shell program for error messages
+ * @line_number: input line number (non-interactive)
+ * @interactive: 1 if interactive mode, 0 otherwise
  *
  * Return: 0 on success, -1 on failure
  */
 
-int fork_and_execute_command(char **argv, char **env, char *progname)
+int fork_and_execute_cmd(char **argv, char **env, char *progname,
+		unsigned long line_number, int interactive)
 {
 	pid_t child;
 	int status;
@@ -30,7 +33,11 @@ int fork_and_execute_command(char **argv, char **env, char *progname)
 	if (child == 0)
 	{
 		execve(argv[0], argv, env);
-		perror(progname);
+
+		if (errno == EACCES)
+			exit(126);
+
+		print_not_found(progname, line_number, argv[0], interactive);
 		exit(127);
 	}
 	if (waitpid(child, &status, 0) == -1)
@@ -38,6 +45,9 @@ int fork_and_execute_command(char **argv, char **env, char *progname)
 		perror("waitpid");
 		return (-1);
 	}
+
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
 
 	return (0);
 }
