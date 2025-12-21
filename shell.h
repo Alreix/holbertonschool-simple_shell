32bf4fffb0319ebfd5d56cedb4ef73f1a2122be0
Alreix <alreixdev@gmail.com>
2025-12-21 23:44:31 +0100

    feat: added handle_line and read command

--- shell.h
+++ shell.h
@@ -6,12 +6,25 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <errno.h>
+#include <stddef.h>
 
 int print_prompt(void);
 
+ssize_t read_command(char **line, size_t *buf_size, int interactive,
+		unsigned long *line_number);
+int handle_line(char *line, char **env, char *progname,
+		unsigned long line_number, int interactive);
+
+void clean_line(char *line);
+int is_blank_line(char *line);
 char **split_line(char *line, char *separator);
 void free_tokens(char **tokens);
 
-int fork_and_execute_command(char **argv, char **env, char *progname);
+int fork_and_execute_cmd(char **argv, char **env, char *progname,
+		unsigned long line_number, int interactive);
+
+void print_not_found(char *progname, unsigned long line_number, char *cmd,
+		int interactive);
 
 #endif
