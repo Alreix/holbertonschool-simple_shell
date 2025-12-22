bfcc720c5972cb6a6ccf03d217a6a518e42c8453
Alreix <alreixdev@gmail.com>
2025-12-22 12:06:24 +0100

    feat(parsing): add line cleanup and argv tokenization helpers

--- parsing.c
+++ parsing.c
@@ -3,6 +3,45 @@
 #include <stdlib.h>
 
 /**
+ * clean_line - removes trailing newline from a line
+ * @line: input line to clean
+ */
+void clean_line(char *line)
+{
+	size_t i = 0;
+
+	if (line == NULL)
+		return;
+
+	while (line[i] != '\0')
+		i++;
+
+	if (i > 0 && line[i - 1] == '\n')
+		line[i - 1] = '\0';
+}
+
+/**
+ * is_blank_line - checks if a line contains only spaces or tabs
+ * @line: input string
+ *
+ * Return: 1 if blank, 0 otherwise
+ */
+int is_blank_line(char *line)
+{
+	int i = 0;
+
+	if (line == NULL)
+		return (1);
+
+	while (line[i] != '\0')
+	{
+		if (line[i] != ' ' && line[i] != '\t')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+/**
  * free_tokens - frees tokens array returned by split_line
  * @tokens: tokens array to free
  */
@@ -12,7 +51,7 @@ void free_tokens(char **tokens)
 }
 
 /**
- * split_line - splits a line into tokens (spaces/tabs supported)
+ * split_line - splits a line into tokens
  * @line: input line to split (will be modified by strtok)
  * @separator: set of delimiters (example: " \t")
  *
