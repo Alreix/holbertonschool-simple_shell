c495550f57b8ae64cad5761b4f93cf2589d12553
Alreix <alreixdev@gmail.com>
2025-12-22 14:57:23 +0100

    feat(header): add prototypes for builtins, PATH resolution, and execution helpers

--- shell.h
+++ shell.h
@@ -9,11 +9,13 @@
 #include <errno.h>
 #include <stddef.h>
 #include <sys/stat.h>
+#include <string.h>
 
 /* prompt.c*/
 int print_prompt(void);
 
 /* loop.c*/
+int exit_cmd(char **argv);
 ssize_t read_command(char **line, size_t *buf_size, int interactive,
 		unsigned long *line_number);
 int handle_line(char *line, char **env, char *progname,
@@ -29,9 +31,12 @@ void free_tokens(char **tokens);
 int fork_and_execute_cmd(char *path, char **argv, char **env);
 
 /* error.c*/
-void print_not_found(char *progname, unsigned long line_number, char *cmd,
-		int interactive);
+void print_not_found(char *progname, unsigned long line_number,
+		char *cmd, int interactive);
+
 /* path.c*/
+int exec_with_path(char **argv, char **env, char *progname,
+		unsigned long line_number, int interactive);
 int file_exists(char *path);
 char *get_env_value(const char *name, char **env);
 char *build_full_path(char *dir, char *cmd);
