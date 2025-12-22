81e4778b3b09ac3da3affab668cbc89768e04657
Alreix <alreixdev@gmail.com>
2025-12-22 12:05:59 +0100

    feat(loop): handle one input line (blank check, split, resolve PATH, exec)

---
+++ loop.c
@@ -0,0 +1,76 @@
+#include "shell.h"
+
+/**
+ * read_command - reads one command line from stdin
+ * @line: address of the buffer used by getline
+ * @buf_size: address of the buffer size for getline
+ * @interactive: 1 if stdin is a terminal, 0 otherwise
+ * @line_number: address of the current input line number
+ *
+ * Return: number of bytes read, or -1 on EOF/error
+ */
+ssize_t read_command(char **line, size_t *buf_size, int interactive,
+		unsigned long *line_number)
+{
+	ssize_t read_line;
+
+	read_line = getline(line, buf_size, stdin);
+	if (read_line == -1)
+	{
+		if (interactive)
+			printf("\n");
+		return (-1);
+	}
+
+	(*line_number)++;
+	clean_line(*line);
+
+	return (read_line);
+}
+
+/**
+ * handle_line - parses and executes a command line
+ * @line: input line (will be modified by strtok)
+ * @env: environment variables
+ * @progname: argv[0] (program name for errors)
+ * @line_number: current input line number
+ * @interactive: 1 if interactive mode, 0 otherwise
+ *
+ * Return: command exit status, or 1 on internal error
+ */
+int handle_line(char *line, char **env, char *progname,
+		unsigned long line_number, int interactive)
+{
+	char **argv;
+	int status;
+	char *path;
+
+	if (is_blank_line(line))
+		return (0);
+
+	argv = split_line(line, " \t");
+	if (argv == NULL)
+		return (1);
+
+	if (argv[0] == NULL || argv[0][0] == '\0' || is_blank_line(argv[0]))
+	{
+		free_tokens(argv);
+		return (0);
+	}
+
+	path = resolve_command(argv[0], env);
+	if (path == NULL)
+	{
+		print_not_found(progname, line_number, argv[0], interactive);
+		free_tokens(argv);
+		return (127);
+	}
+	status = fork_and_execute_cmd(path, argv, env);
+
+	free_tokens(argv);
+
+	if (status == -1)
+		return (1);
+
+	return (status);
+}
