04a3696790c7faefe2e59fa25636cbca10254fb5
Alreix <alreixdev@gmail.com>
2025-12-23 18:37:28 +0100

    feat(errors): standardize shell-style error output (not found, permission)

--- error.c
+++ error.c
@@ -21,3 +21,25 @@ void print_not_found(char *progname, unsigned long line_number,
 	else
 		fprintf(stderr, "%s: %lu: %s: not found\n", progname, line_number, cmd);
 }
+
+/**
+ * print_permission_denied - prints a permission denied error message
+ * @progname: name of the shell program
+ * @line_number: command line number (non-interactive)
+ * @cmd: command that failed
+ * @interactive: 1 if interactive mode, 0 otherwise
+ */
+void print_permission_denied(char *progname, unsigned long line_number,
+			     char *cmd, int interactive)
+{
+	if (progname == NULL || cmd == NULL || cmd[0] == '\0')
+		return;
+
+	if (interactive)
+		fprintf(stderr, "%s: %s: Permission denied\n", progname, cmd);
+	else
+	{
+		fprintf(stderr, "%s: %lu: %s: Permission denied\n",
+				progname, line_number, cmd);
+	}
+}
