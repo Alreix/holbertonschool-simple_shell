eab5dd553ffb8bd8ff48bb34a807007114df8d8c
Alreix <alreixdev@gmail.com>
2025-12-24 15:24:08 +0100

    fix(shell.h): remove file_exists

--- shell.h
+++ shell.h
@@ -9,6 +9,7 @@
 #include <errno.h>
 #include <stddef.h>
 #include <sys/stat.h>
+#include <string.h>
 
 /* prompt.c*/
 int print_prompt(void);
@@ -29,12 +30,20 @@ void free_tokens(char **tokens);
 int fork_and_execute_cmd(char *path, char **argv, char **env);
 
 /* error.c*/
-void print_not_found(char *progname, unsigned long line_number, char *cmd,
-		int interactive);
+void print_not_found(char *progname, unsigned long line_number,
+		char *cmd, int interactive);
+void print_permission_denied(char *progname, unsigned long line_number,
+			     char *cmd, int interactive);
+
 /* path.c*/
-int file_exists(char *path);
+int exec_with_path(char **argv, char **env, char *progname,
+		unsigned long line_number, int interactive);
 char *get_env_value(const char *name, char **env);
-char *build_full_path(char *dir, char *cmd);
 char *resolve_command(char *cmd, char **env);
+
+/* path_util.c*/
+char *resolve_slash_cmd(char *cmd);
+char *build_full_path(char *dir, char *cmd);
+char *search_in_path(char *cmd, char *path_value);
 
 #endif
