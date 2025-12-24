9a61665bbec9f08189ae9c0b5b74005e1a8f2e83
Alreix <alreixdev@gmail.com>
2025-12-24 11:58:17 +0100

    perf(path_utils.c): move build_full_path for better readability

--- path_utils.c
+++ path_utils.c
@@ -11,6 +11,30 @@ char *resolve_slash_cmd(char *cmd)
 	return (NULL);
 }
 
+/**
+ * build_full_path - build "dir/cmd"
+ * @dir: directory
+ * @cmd: command
+ *
+ * Return: malloc'd full path, or NULL
+ */
+char *build_full_path(char *dir, char *cmd)
+{
+	size_t len;
+	char *full_path;
+
+	if (dir == NULL || cmd == NULL)
+		return (NULL);
+
+	len = strlen(dir) + 1 + strlen(cmd) + 1;
+	full_path = malloc(len);
+	if (full_path == NULL)
+		return (NULL);
+
+	sprintf(full_path, "%s/%s", dir, cmd);
+	return (full_path);
+}
+
 char *search_in_path(char *cmd, char *path_value)
 {
 	char *path_copy, *dir, *full_path;
