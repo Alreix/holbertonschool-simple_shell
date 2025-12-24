ef11018c07c5d6d8fda6f2c469d27d683e850de1
Alreix <alreixdev@gmail.com>
2025-12-24 13:25:33 +0100

    add(builtin.c): add the check if argv[1] == NULL in env_cmd

--- builtin.c
+++ builtin.c
@@ -28,7 +28,7 @@ int env_cmd(char **argv)
 	if (argv == NULL || argv[0] == NULL)
 		return (0);
 
-	if (strcmp(argv[0], "env") == 0)
+	if (strcmp(argv[0], "env") == 0 && argv[1] == NULL)
 		return (1);
 
 	return (0);
