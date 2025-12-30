e028a728bb12a7ce52feda152184850820279004
Alreix <alreixdev@gmail.com>
2025-12-30 19:57:59 +0100

    fix(prompt.c): change the error message type

--- error.c
+++ error.c
@@ -3,16 +3,16 @@
 /**
  * print_not_found - prints a "not found" error message
  * @progname: program name (argv[0])
- * @line_number: input line number (non-interactive)
- * @cmd: command name that was not found
+ * @line_number: input line number (non-interactive)(unused here)
+ * @cmd: command name that was not found (unused here)
  */
 void print_not_found(char *progname, unsigned long line_number, char *cmd)
 {
-	if (progname == NULL || cmd == NULL)
+	(void)line_number;
+	(void)cmd;
+
+	if (progname == NULL)
 		return;
 
-	if (cmd[0] == '\0')
-		return;
-
-	fprintf(stderr, "%s: %lu: %s: not found\n", progname, line_number, cmd);
+	perror(progname);
 }
