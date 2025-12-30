4cdfb88ca804510242e8d4b2ad0f915969f5bf57
Alreix <alreixdev@gmail.com>
2025-12-30 21:13:36 +0100

    fix(error.c): readd the error message

--- error.c
+++ error.c
@@ -3,16 +3,16 @@
 /**
  * print_not_found - prints a "not found" error message
  * @progname: program name (argv[0])
- * @line_number: input line number (non-interactive)(unused here)
- * @cmd: command name that was not found (unused here)
+ * @line_number: input line number (non-interactive)
+ * @cmd: command name that was not found
  */
 void print_not_found(char *progname, unsigned long line_number, char *cmd)
 {
-	(void)line_number;
-	(void)cmd;
-
-	if (progname == NULL)
+	if (progname == NULL || cmd == NULL)
 		return;
 
-	perror(progname);
+	if (cmd[0] == '\0')
+		return;
+
+	fprintf(stderr, "%s: %lu: %s: not found\n", progname, line_number, cmd);
 }
