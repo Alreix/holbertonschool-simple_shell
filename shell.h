e9a00e9456b5bc815201b112b6fc4727d9e44ea3
Alreix <alreixdev@gmail.com>
2025-12-30 22:41:11 +0100

    fix(shell.h): add the ; at the end of print_permission_denied

--- shell.h
+++ shell.h
@@ -20,5 +20,5 @@ int fork_and_execute_cmd(char *cmd, char **env, char *progname,
 
 void print_not_found(char *progname, unsigned long line_number, char *cmd);
 void print_permission_denied(char *progname, unsigned long line_number,
-		char *cmd)
+		char *cmd);
 #endif
