7be3cbc5a1191f51e5876b19e0cf72af7976675a
Alreix <alreixdev@gmail.com>
2025-12-30 19:28:02 +0100

    add(shell.h): add prototypes functions of simple_shell_0.1

--- shell.h
+++ shell.h
@@ -1,5 +1,24 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stddef.h>
+
+
+int print_prompt(void);
+
+void clean_line(char *line);
+int is_blank_line(char *line);
+
+int fork_and_execute_cmd(char *cmd, char **env, char *progname,
+		unsigned long line_number);
+
+void print_not_found(char *progname, unsigned long line_number, char *cmd);
+
 
 #endif
