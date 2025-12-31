ba6ef4792771f168a5ddf38cfdd3c09ba75bcdd7
Alreix <alreixdev@gmail.com>
2025-12-31 13:05:37 +0100

    perf(error): rename line number to line_count and pass line number in int type

--- error.c
+++ error.c
@@ -3,12 +3,10 @@
 /**
  * print_not_found - prints a "not found" error message
  * @progname: program name (argv[0])
- * @line_number: input line number (non-interactive)
+ * @line_count: input line number (non-interactive)
  * @cmd: command name that was not found
- * @interactive: if the shell is in interactive mode
  */
-void print_not_found(char *progname, unsigned long line_number,
-		char *cmd, int interactive)
+void print_not_found(char *progname, int line_count, char *cmd)
 {
 	if (progname == NULL || cmd == NULL)
 		return;
@@ -16,30 +14,21 @@ void print_not_found(char *progname, unsigned long line_number,
 	if (cmd[0] == '\0')
 		return;
 
-	if (interactive)
-		fprintf(stderr, "%s: %s: not found\n", progname, cmd);
-	else
-		fprintf(stderr, "%s: %lu: %s: not found\n", progname,
-			line_number, cmd);
+	fprintf(stderr, "%s: %d: %s: not found\n", progname,
+			line_count, cmd);
 }
 
 /**
  * print_permission_denied - prints a permission denied error message
  * @progname: name of the shell program
- * @line_number: command line number (non-interactive)
+ * @line_count: command line number (non-interactive)
  * @cmd: command that failed
- * @interactive: if the shell is in interactive mode
  */
-void print_permission_denied(char *progname, unsigned long line_number,
-			     char *cmd, int interactive)
+void print_permission_denied(char *progname, int line_count, char *cmd)
 {
 	if (progname == NULL || cmd == NULL || cmd[0] == '\0')
 		return;
 
-	if (interactive)
-		fprintf(stderr, "%s: %s: permission denied\n", progname, cmd);
-
-	else
-		fprintf(stderr, "%s: %lu: %s: permission denied\n", progname,
-			line_number, cmd);
+	fprintf(stderr, "%s: %d: %s: permission denied\n", progname,
+			line_count, cmd);
 }
