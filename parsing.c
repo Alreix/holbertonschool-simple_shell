bd8ef814df772b80f0d5a9d2d5af1d0df44400cb
Alreix <alreixdev@gmail.com>
2025-12-31 18:24:50 +0100

    add(parsing): split_line for parse the line (simple sehll 0.2)

--- parsing.c
+++ parsing.c
@@ -1,4 +1,6 @@
 #include "shell.h"
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * clean_line - removes trailing newline from a line
@@ -38,4 +40,58 @@ int is_blank_line(char *line)
 		i++;
 	}
 	return (1);
+}
+/**
+ * free_tokens - frees tokens array returned by split_line
+ * @tokens: tokens array to free
+ */
+void free_tokens(char **tokens)
+{
+	free(tokens);
+}
+
+/**
+ * split_line - splits a line into tokens
+ * @line: input line to split (will be modified by strtok)
+ * @separator: set of delimiters (example: " \t")
+ *
+ * Return: malloc'd tokens array, or NULL on failure
+ */
+char **split_line(char *line, char *separator)
+{
+	char *token;
+	char **tokens, **new_tokens;
+	int buffer_size = 10, i = 0;
+
+	tokens = malloc(sizeof(char *) * buffer_size);
+	if (tokens == NULL)
+		return (NULL);
+
+	token = strtok(line, separator);
+	if (token == NULL)
+	{
+		free(tokens);
+		return (NULL);
+	}
+	while (token != NULL)
+	{
+		if (i + 1 >= buffer_size)
+		{
+			buffer_size *= 2;
+			new_tokens = realloc(tokens, sizeof(char *) * buffer_size);
+			if (new_tokens == NULL)
+			{
+				free(tokens);
+				return (NULL);
+			}
+			tokens = new_tokens;
+		}
+
+		tokens[i] = token;
+		i++;
+		token = strtok(NULL, separator);
+	}
+	tokens[i] = NULL;
+
+	return (tokens);
 }
