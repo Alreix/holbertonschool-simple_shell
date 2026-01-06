4acc4ca46a4eae5df119543ecea68ac2f6f74fcf
Alreix <alreixdev@gmail.com>
2026-01-06 09:37:57 +0100

    perf(parsing): remove obsolete verification on clean_line

--- parsing.c
+++ parsing.c
@@ -8,7 +8,7 @@
  */
 void clean_line(char *line)
 {
-	size_t i = 0, start = 0, end;
+	size_t i = 0;
 
 	if (line == NULL)
 		return;
@@ -19,26 +19,6 @@ void clean_line(char *line)
 	if (i > 0 && line[i - 1] == '\n')
 	{
 		line[i - 1] = '\0';
-		i--;
-	}
-
-	end = i;
-	while (end > 0 && (line[end - 1] == ' ' || line[end - 1] == '\t'))
-		end--;
-	line[end] = '\0';
-
-	while (line[start] == ' ' || line[start] == '\t')
-		start++;
-
-	if (start > 0)
-	{
-		i = 0;
-		while (line[start + i])
-		{
-			line[i] = line[start + i];
-			i++;
-		}
-		line[i] = '\0';
 	}
 }
 
